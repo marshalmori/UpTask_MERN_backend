3c76c39e373f82d32b4247fde5146d7f41b3a930
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-05 22:22:56 -0300

    Initial commit'

---
+++ models/Usuario.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+
+const usuarioSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    token: {
+      type: String,
+    },
+    confirmado: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+usuarioSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+});
+
+usuarioSchema.methods.comprobarPassword = async function (passwordFormulario) {
+  return await bcryptjs.compare(passwordFormulario, this.password);
+};
+
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+export default Usuario;
